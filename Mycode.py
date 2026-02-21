beb26b39a74b9a985eca8787a4a1700231789fa0
AdityaModi001 <adityamodi108@gmail.com>
2026-02-21 16:57:50 +0530

    This is the first commit before DVC

---
+++ Mycode.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+
+# create a sample dataframe with column name
+
+data = {
+    'name':['Aditya','bob','charlie'],
+    'age':[21,42,12],
+    'city':['New York', 'Los Angeles','chicago']
+}
+df = pd.DataFrame(data)
+
+# # adding a new column in a row
+# new_row1 = {
+#     'name':"v2",
+#     'age':34,
+#     'city':"city1"
+# }
+# df.loc[len(df.index)] = new_row1
+#
+# new_row2 = {
+#     'name':"v2",
+#     'age':34,
+#     'city':"city2"
+# }
+# df.loc[len(df.index)] = new_row2
+
+
+# Ensure the data directory exists at the root level
+data_dir = 'data'
+os.mkdir(data_dir)
+
+# define the file path
+file_path = os.path.join(data_dir,"sample_data.csv")
+
+# safe the data frame to csv file, including column names
+df.to_csv(file_path,index = False)
+
+print(f"CSV file saved to {file_path}")
